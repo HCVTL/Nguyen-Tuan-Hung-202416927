f5935bf25fa2b002bd5f16b186980563f64705ff
Nguyen Tuan Hung <anhduc1612000@gmail.com>
2026-03-31 14:19:40 +0700

    Lab02

---
+++ src/Aims.java
@@ -0,0 +1,21 @@
+public class Aims {
+    public static void main(String args[]) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc d1 = new DigitalVideoDisc("Naruto", "Anime", "Hung", 90, 100);
+        DigitalVideoDisc d2 = new DigitalVideoDisc("Tho Oi", "Drama", "Tran Thanh", 90, 50);
+
+        cart.addDigitalVideoDisc(d1);
+        cart.addDigitalVideoDisc(d2);
+
+        cart.printCart();
+
+        System.out.println(cart.totalCost());
+
+        cart.removeDigitalVideoDisc(d1);
+
+        cart.printCart();
+
+
+    }
+}
---
+++ src/DigitalVideoDisc.java
@@ -0,0 +1,31 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() { return title;}
+    public String getCategory() {return category;}
+    public String getDirector() {return director;}
+    public int getLength() {return length;}
+    public float getCost() {return cost;}
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        DigitalVideoDisc other = (DigitalVideoDisc) obj;
+        return this.title != null && this.title.equals(other.title);
+    }
+
+}
