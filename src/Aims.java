0bc267a76b611a7ba80bfa46239013a5e048bc47
Nguyen Tuan Hung <anhduc1612000@gmail.com>
2026-04-04 22:10:19 +0700

    Bai lab02

--- src/Aims.java
+++ src/Aims.java
@@ -2,8 +2,8 @@ public class Aims {
     public static void main(String args[]) {
         Cart cart = new Cart();
 
-        DigitalVideoDisc d1 = new DigitalVideoDisc("Naruto", "Anime", "Hung", 90, 100);
-        DigitalVideoDisc d2 = new DigitalVideoDisc("Tho Oi", "Drama", "Tran Thanh", 90, 50);
+        DigitalVideoDisc d1 = new DigitalVideoDisc("Naruto", "Anime", "Hung", 90, 100f);
+        DigitalVideoDisc d2 = new DigitalVideoDisc("Tho Oi", "Drama", "Tran Thanh", 90, 50f);
 
         cart.addDigitalVideoDisc(d1);
         cart.addDigitalVideoDisc(d2);
